0ad6050c0a47bb2db876c83b9b0a1106441b3975
Steve Jones <sjones@sqlservercentral.com>
2022-09-07 16:44:46 -0600

    code updates for prompt

---
+++ Prompt/PromptIntro.sql
@@ -0,0 +1,22 @@
+/*
+Access Group
+
+SQL Prompt
+*/
+
+-- use westwind
+USE Westwind
+GO
+
+-- select * from 
+-- choose order details
+
+
+GO
+-- rewrite, select from order details, ij join with products, on
+
+GO
+-- expand the *
+
+-- CTRL -Z
+-- expand with column picker
